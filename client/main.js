254d4638454ed96e6f342db6e03288d99c0f0a98
Ovidiu Bodea <antarexis@gmail.com>
2016-06-23 00:03:58 +0300

    class based components

--- client/main.js
+++ client/main.js
@@ -2,7 +2,7 @@
 
 
 // Import the React library
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import ImageList from './components/image_list';
@@ -10,12 +10,15 @@ import ImageList from './components/image_list';
 
 
 // Create a component
-const App = () => {
-  return(
-    <div>
-      <ImageList />
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return(
+      <div>
+        <ImageList />
+      </div>
+    );
+  }
 };
 
 // Render this component to the screen.
