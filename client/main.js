e94d18cbd3ed7aefeb202bb2a1a83ea182f07a1c
Ovidiu Bodea <antarexis@gmail.com>
2016-06-22 15:10:56 +0300

    created react component

--- client/main.js
+++ client/main.js
@@ -2,9 +2,15 @@
 
 
 // Import the React library
-
+import React from 'react';
 
 // Create a component
-
+const App = () => {
+  return(
+    <div>
+      React App #2
+    </div>
+  );
+};
 
 // Render this component to the screen.
