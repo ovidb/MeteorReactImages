2093d44a3b5c4fe9468c9291d2db9872274e09b7
Ovidiu Bodea <antarexis@gmail.com>
2016-06-26 22:03:42 +0300

    implemented ImageScore

--- client/components/image_detail.js
+++ client/components/image_detail.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ImageScore from './image_score';
 
 const ImageDetail =(props) => {
   //props.image => this is the image object
@@ -15,6 +16,8 @@ const ImageDetail =(props) => {
         <h4 className="media-heading">
           {props.image.title}
         </h4>
+        <p>{props.image.description}</p>
+        <ImageScore ups={props.image.ups} downs={props.image.downs} />
       </div>
     </li>
 
